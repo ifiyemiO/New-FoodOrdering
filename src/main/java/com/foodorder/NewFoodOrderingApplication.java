09cd7721bb09b886202c5bcdf23d4fdf42edb891
ifiyemiO <ifiyemi.owi@keyin.com>
2024-12-07 10:40:53 -0330

    Initial commit

---
+++ src/main/java/com/foodorder/NewFoodOrderingApplication.java
@@ -0,0 +1,13 @@
+package com.foodorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewFoodOrderingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewFoodOrderingApplication.class, args);
+	}
+
+}

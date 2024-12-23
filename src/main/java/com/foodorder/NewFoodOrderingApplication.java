d1dbda432568dcd67c353729ccd4acd632ef363a
ifiyemiO <ifiyemi.owi@keyin.com>
2024-12-23 01:36:32 -0330

    docker update

--- src/main/java/com/foodorder/NewFoodOrderingApplication.java
+++ src/main/java/com/foodorder/NewFoodOrderingApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NewFoodOrderingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NewFoodOrderingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NewFoodOrderingApplication.class, args);
+    }
 
 }
--- src/test/java/com/foodorder/NewFoodOrderingApplicationTests.java
+++ src/test/java/com/foodorder/NewFoodOrderingApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class NewFoodOrderingApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
